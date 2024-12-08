ad5b4b4c4eedded010d8b0af8d6a9f40025028ea
shuiwenbubu <deschengche@gmail.com>
2024-12-08 21:44:00 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '286c4215e43c455586cb462ef539c0f5',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Ji Chengran',
+  domain: 'jichengran.com',
+  author: 'Ji Chengran',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
